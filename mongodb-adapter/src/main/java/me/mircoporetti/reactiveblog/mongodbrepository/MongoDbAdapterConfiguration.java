4151ffd06bcb55ad023fa316f7d0d13e17c36bbc
Mirco Poretti <mircoporetti@gmail.com>
2022-04-30 23:43:59 +0200

    Removed unused import

--- mongodb-adapter/src/main/java/me/mircoporetti/reactiveblog/mongodbrepository/MongoDbAdapterConfiguration.java
+++ mongodb-adapter/src/main/java/me/mircoporetti/reactiveblog/mongodbrepository/MongoDbAdapterConfiguration.java
@@ -1,6 +1,5 @@
 package me.mircoporetti.reactiveblog.mongodbrepository;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
